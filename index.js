988dd7d50a867b985c0283739559055c4aea21a9
josue <83614619+josuefeo123@users.noreply.github.com>
2022-01-25 15:22:41 -0500

    Curso_De_Manipulacion_De_Arrays_Part_2
    Curso_De_Manipulacion_De_Arrays_Part_2

---
+++ index.js
@@ -0,0 +1,31 @@
+console.log(`TODO LO APRENDIDO, EJEMPLO IF ELSE FOR, NODE TIENE INTEGRADO `);
+console.info("Info"); 
+console.error("Error"); 
+console.warn("Warn"); 
+//
+const os = require('os');
+console.log(os.hostname());
+console.log(os.networkInterfaces());
+console.log(os.tmpdir())
+console.log(os.homedir()) 
+console.log(os.arch()); 
+console.log(os.platform());
+console.log(os.cpus());
+console.log(os.constants());
+console.log(os.freemem());
+
+        
+
+
+ 
+
+
+
+
+/*
+
+        File system:
+                   El fs módulo permite interactuar con el sistema de archivos de una manera modelada en las funciones POSIX estándar.         
+
+
+*/
